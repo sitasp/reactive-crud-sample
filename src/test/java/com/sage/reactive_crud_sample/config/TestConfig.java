6fdb747b8e23cd329328ee0248107d4e9763524c
sitasp <44961824+sitasp@users.noreply.github.com>
2024-06-27 12:56:02 +0530

    Create TestConfig.java

---
+++ src/test/java/com/sage/reactive_crud_sample/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.sage.reactive_crud_sample.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+@TestConfiguration
+public class TestConfig {
+
+    // no use of this bean
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        return initializer;
+    }
+}
