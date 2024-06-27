08f9a3d38f65abfce167a6d2fea63844d1bbcf84
sitasp <44961824+sitasp@users.noreply.github.com>
2024-06-27 15:22:56 +0530

    Update ReactiveCrudSampleApplicationTests.java

--- src/test/java/com/sage/reactive_crud_sample/ReactiveCrudSampleApplicationTests.java
+++ src/test/java/com/sage/reactive_crud_sample/ReactiveCrudSampleApplicationTests.java
@@ -1,12 +1,7 @@
 package com.sage.reactive_crud_sample;
 
-import io.r2dbc.spi.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootTest
 class ReactiveCrudSampleApplicationTests {
